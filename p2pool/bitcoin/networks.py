5946ed41b6fa27e925fd8cd3b23efd8259bddb02
Alex Movsisyan <alex@Alexs-MacBook-Pro.local>
2014-01-10 21:37:20 +0400

    Feathercoin code update

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -198,20 +198,17 @@ nets = dict(
             'feathercoinaddress' in (yield bitcoind.rpc_help()) and
             not (yield bitcoind.rpc_getinfo())['testnet']
         )),
-        SUBSIDY_FUNC=lambda height: 200 * 100000000 >> (height + 1) // 3360000,
+        SUBSIDY_FUNC=lambda height: 200*100000000 >> (height + 1)//3360000,
         POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
         BLOCK_PERIOD=150, # s
         SYMBOL='FTC',
-        CONF_FILE_FUNC=lambda: os.path.join(
-            os.path.join(os.environ['APPDATA'], 'Feathercoin') if platform.system() == 'Windows' else os.path.expanduser(
-                '~/Library/Application Support/Feathercoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.feathercoin'),
-            'feathercoin.conf'),
-        BLOCK_EXPLORER_URL_PREFIX='http://cryptocoinexplorer.com:5750/block/',
-        ADDRESS_EXPLORER_URL_PREFIX='http://cryptocoinexplorer.com:5750/address/',
-        TX_EXPLORER_URL_PREFIX='http://cryptocoinexplorer.com:5750/transaction/',
-        SANE_TARGET_RANGE=(2 ** 256 // 1000000000 - 1, 2 ** 256 // 1000 - 1),
-        DUMB_SCRYPT_DIFF=2 ** 16,
-        DUST_THRESHOLD=1e8,
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Feathercoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Feathercoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.feathercoin'), 'feathercoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://explorer.feathercoin.com/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://explorer.feathercoin.com/address/',
+        TX_EXPLORER_URL_PREFIX='http://explorer.feathercoin.com/tx/',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+        DUST_THRESHOLD=0.03e8,
     ),
     digitalcoin=math.Object(
         P2P_PREFIX='fbc0b6db'.decode('hex'),
